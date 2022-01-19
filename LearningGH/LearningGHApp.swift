b09901e83a2ce0afca05d89d0fcd7c0d9deef61c
Emir-Danisik <emir.danisik@hotmail.com>
2022-01-18 20:49:53 -0600

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Emir Danisik on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
